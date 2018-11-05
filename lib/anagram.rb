167f7fdd1e7328c638c30a55c4ee5dd936cc99de
Andrew Trent <andrewtrentdesign@gmail.com>
2018-11-05 16:36:35 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array) 
+    compare_one = @word.split("")
+    compare_two = array.map |word| do
+      sub_array = [] 
+      sub_array << word.split("")
+      sub_array
+    end
+    compare_two.each do |split_word|
+      array[split_word.index] if split_word == compare_two
+    end
+
+end
